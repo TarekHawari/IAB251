a33037823fd6bfaa8b243700070591a63f70c318
Ronin <n11302844@qut.edu.au>
2026-05-19 21:33:48 +1000

    stashed changes

--- ExternalSystems/HRSystem/HRSystem/obj/Debug/net8.0/HRSystem.AssemblyInfo.cs
+++ ExternalSystems/HRSystem/HRSystem/obj/Debug/net8.0/HRSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ffea78514209e2dca59cc4a8dfb1762978660b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f9b22480b81a79b49b2269e2cc1ff1ddff9fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
