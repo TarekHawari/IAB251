2176bbfcb78cf84b29be3b584a930a9f60e5c5c7
Ronin <n11302844@qut.edu.au>
2026-05-21 14:15:46 +1000

    fixed some UI Issues, and implemented cleaner navigation routing

--- IAB251InterportCargoAssignment2Grp21/obj/Debug/net9.0/IAB251InterportCargoAssignment2Grp21.AssemblyInfo.cs
+++ IAB251InterportCargoAssignment2Grp21/obj/Debug/net9.0/IAB251InterportCargoAssignment2Grp21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IAB251InterportCargoAssignment2Grp21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d004b3e9a44a43bc0aa42d72c321593ef0cc39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f59690a21b56ebc862523954011a1ace0c1667")]
 [assembly: System.Reflection.AssemblyProductAttribute("IAB251InterportCargoAssignment2Grp21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IAB251InterportCargoAssignment2Grp21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
